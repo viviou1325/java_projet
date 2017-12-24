2f835286e088b89840f82758e84bf952d76ad094
Takahiruma <geoffreydan@wanadoo.fr>
2017-12-24 19:57:07 +0100

    le bouton est invisible et fonctionne

--- Guitare/src/guitare/bouton.java
+++ Guitare/src/guitare/bouton.java
@@ -19,20 +19,25 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 public class bouton implements ActionListener{
     Fenetre fenetre;
   public bouton(Fenetre j){
-      JButton button1=new JButton("oui"); 
+      JButton button1=new JButton("oui");
+      button1.setActionCommand("oui");
       button1.addActionListener(this);
-      button1.setBounds(50,30,150,40);
+      
+      button1.setBounds(50,30,75,40);
+      button1.setBorder(null);
+      button1.setBorderPainted(false);
+      button1.setContentAreaFilled(false);
+      button1.setOpaque(false);
       
       j.setLayout(new BorderLayout());
       j.add(button1);
+      
 //      j.add(button1,BorderLayout.CENTER);
   }
 
     public void actionPerformed(ActionEvent evenement){
-        if(evenement.getActionCommand().equals("menu_quitter")){
-            if( JOptionPane.showConfirmDialog(null,"Voulez vous quitter?","Quitter",JOptionPane.YES_NO_OPTION,JOptionPane.ERROR_MESSAGE) ==  JOptionPane.YES_OPTION){
-            System.exit(0);
-            }
+        if(evenement.getActionCommand().equals("oui")){
+            System.out.println("je fonctionne");
         }
         
         
