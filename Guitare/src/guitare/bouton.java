3cf41f3260c8bc6f81b533198b088346e7045dfc
Takahiruma <geoffreydan@wanadoo.fr>
2017-12-27 13:06:05 +0100

    je vais faire un tableau de bouton mais pour l'instant ça ne marche pas

--- Guitare/src/guitare/bouton.java
+++ Guitare/src/guitare/bouton.java
@@ -17,43 +17,62 @@ import javax.swing.filechooser.FileNameExtensionFilter;
  * @author taka
  */
 public class bouton implements ActionListener{
-    Fenetre fenetre;
+    ImageIcon select,unselect;
+    boolean selected;
+    private Container content;
   public bouton(Fenetre j){
-      ImageIcon icon= new ImageIcon("C:\\Users\\taka\\Desktop\\java\\java_projet\\Guitare\\src\\image\\icon.png");
-      ImageIcon icon_s= new ImageIcon("C:\\Users\\taka\\Desktop\\java\\java_projet\\Guitare\\src\\image\\icon_selected.png");
-      JButton button1=new JButton(icon);
-      button1.setActionCommand("oui");
+      try{
+      ImageIcon unselect= new ImageIcon("C:\\Users\\taka\\Desktop\\java\\java_projet\\Guitare\\src\\image\\icon.png");
+      ImageIcon select= new ImageIcon("C:\\Users\\taka\\Desktop\\java\\java_projet\\Guitare\\src\\image\\icon_selected.png");
+      }catch(Exception e){
+          e.printStackTrace();
+      }
+      content = j.getContentPane();
+      content.setBackground(Color.white);
+      content.setLayout(new GridLayout(5,6));
+      JButton button[][]=new JButton[5][6];
       
-      button1.addActionListener(this);
+      for(int i = 0; i< button.length; ++i)
+          for(int k = 0; k<6;++k){
+              button[i][k].setIcon(unselect);//pb
+              content.add(button[i][k]);
+          }
+//      button.setActionCommand("ou");
+//      button.addActionListener(this);
+//      
+//      button.setBounds(80,80,75,40);
+//      button.setBorder(null);
+//      button.setBorderPainted(false);
+//      button.setContentAreaFilled(false);
+//      button.setOpaque(false);
+//      
+//      button1.setBounds(80,30,75,40);
+//      button1.setBorder(null);
+//      button1.setBorderPainted(false);
+//      button1.setContentAreaFilled(false);
+//      button1.setOpaque(false);
       
-      JButton button=new JButton(icon_s);
-      button.setActionCommand("ou");
-      button.addActionListener(this);
-      
-      button.setBounds(80,80,75,40);
-      button.setBorder(null);
-      button.setBorderPainted(false);
-      button.setContentAreaFilled(false);
-      button.setOpaque(false);
-      
-      button1.setBounds(80,30,75,40);
-      button1.setBorder(null);
-      button1.setBorderPainted(false);
-      button1.setContentAreaFilled(false);
-      button1.setOpaque(false);
-      
-      j.setLayout(new BorderLayout());
-      j.add(button1);
-      j.add(button);
+//      j.setLayout(new BorderLayout());
+//      j.add(button1);
+//      j.add(button);
       
   }
 
     public void actionPerformed(ActionEvent evenement){
         if(evenement.getActionCommand().equals("oui")){
-            System.out.println("je fonctionne");
+//            if(!selected()){
+//                
+//            }
         }
         
         
         
     }
+    
+//    private boolean selected(JButton b){
+//        boolean s = false;
+//        if()
+//        
+//        return s;
+//    }
 }
